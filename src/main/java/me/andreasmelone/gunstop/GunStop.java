fa4773f4de1edde05e4a0125da29db878e052665
RaydanOMGr <andreykelbler@outlook.de>
2023-05-30 11:19:36 +0200

    Fix the config creation and loading order

--- src/main/java/me/andreasmelone/gunstop/GunStop.java
+++ src/main/java/me/andreasmelone/gunstop/GunStop.java
@@ -11,12 +11,13 @@ public final class GunStop extends JavaPlugin {
     public final Logger LOGGER = LogManager.getLogger();
     public Magazine magazine = new Magazine(this);
     public ResourcePackManager resourcePackManager = new ResourcePackManager(this);
-    FileConfiguration config = getConfig();
+    FileConfiguration config;
 
     @Override
     public void onEnable() {
         // load the config, the file already exists
         saveDefaultConfig();
+        config  = getConfig();
         // register the event listener
         getServer().getPluginManager().registerEvents(new GunEvents(this), this);
         getServer().getPluginManager().registerEvents(new GunStopResourcePackEvents(this), this);
