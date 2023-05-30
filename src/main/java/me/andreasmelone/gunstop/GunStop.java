baa686d6d6cba345e0058c0f3e80fcd7ea0925e1
RaydanOMGr <andreykelbler@outlook.de>
2023-05-30 11:22:33 +0200

    Fix imports

--- src/main/java/me/andreasmelone/gunstop/GunStop.java
+++ src/main/java/me/andreasmelone/gunstop/GunStop.java
@@ -1,7 +1,6 @@
 package me.andreasmelone.gunstop;
 
 import me.andreasmelone.gunstop.functions.Magazine;
-import me.andreasmelone.gunstop.functions.ResourcePackManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,7 +9,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class GunStop extends JavaPlugin {
     public final Logger LOGGER = LogManager.getLogger();
     public Magazine magazine = new Magazine(this);
-    public ResourcePackManager resourcePackManager = new ResourcePackManager(this);
     FileConfiguration config;
 
     @Override
